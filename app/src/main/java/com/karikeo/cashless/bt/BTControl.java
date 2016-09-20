48dbb90465d4e06374a52af0007bd7ca3bdcb85a
Muradov Roman-B14423 <roman.muradov@nxp.com>
2016-09-20 21:19:18 +0300

    Temporary Commit. Work in progress.

--- app/src/main/java/com/karikeo/cashless/bt/BTControl.java
+++ app/src/main/java/com/karikeo/cashless/bt/BTControl.java
@@ -1,4 +1,22 @@
 package com.karikeo.cashless.bt;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothDevice;
+
 public class BTControl {
+    public static final int REQUEST_ENABLE_BT = 0x922625;
+
+    private Activity mActivity;
+    private String mBTId;
+
+    public BTControl(Activity activity, String btID){
+        mActivity = activity;
+        mBTId = btID;
+    }
+
+    public void openConnection(){
+
+    }
+
+
 }
--- app/src/main/java/com/karikeo/cashless/ui/MainActivity.java
+++ app/src/main/java/com/karikeo/cashless/ui/MainActivity.java
@@ -1,5 +1,7 @@
 package com.karikeo.cashless.ui;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,11 +11,16 @@ import android.widget.Toast;
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.karikeo.cashless.R;
+import com.karikeo.cashless.bt.BTControl;
 import com.karikeo.cashless.ui.barcode.BarcodeCaptureActivity;
+
+import java.util.Set;
 
 public class MainActivity extends ProgressBarActivity {
     private static final int RC_BARCODE_CAPTURE = 9001;
     private TextView balance;
+
+    private BTControl btControl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +51,8 @@ public class MainActivity extends ProgressBarActivity {
                 Toast.makeText(this, String.format(getString(R.string.barcode_error),
                         CommonStatusCodes.getStatusCodeString(resultCode)), Toast.LENGTH_LONG).show();
             }
+        } else if (requestCode == BTControl.REQUEST_ENABLE_BT){
+                btControl
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
@@ -55,6 +64,7 @@ public class MainActivity extends ProgressBarActivity {
     }
 
     private void connect(String id) {
+/*
         showProgress(R.string.connecting);
         balance.postDelayed(new Runnable() {
             @Override
@@ -62,6 +72,15 @@ public class MainActivity extends ProgressBarActivity {
                 onConnect();
             }
         }, 2000);
+*/
+        if (!BluetoothAdapter.checkBluetoothAddress(id)){
+            //Oops wrong address.
+            return;
+        }
+
+        btControl = new BTControl(this, id);
+        btControl.openConnection();
+
     }
 
     public void onConnect() {
