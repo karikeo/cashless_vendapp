e7b4b7d2233cfeef82a23ae63896cf239c153cd0
Muradov Roman-B14423 <roman.muradov@nxp.com>
2016-11-23 17:36:57 +0300

    BT: close connection when income message come.

--- app/src/main/java/com/karikeo/cashless/ui/MainActivity.java
+++ app/src/main/java/com/karikeo/cashless/ui/MainActivity.java
@@ -143,6 +143,8 @@ public class MainActivity extends ProgressBarActivity {
                 //finalDb.close();
 
                 updateBalance(tr);
+
+                blueToothControl.closeConnection();
             }
         });
     }
