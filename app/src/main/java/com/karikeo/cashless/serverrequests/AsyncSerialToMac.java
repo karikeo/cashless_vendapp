841efef6f6c08b4ab0b7d2f3c932d772788a4bcf
Muradov Roman-B14423 <roman.muradov@nxp.com>
2017-01-19 19:58:05 +0300

    Initial

---
+++ app/src/main/java/com/karikeo/cashless/serverrequests/AsyncSerialToMac.java
@@ -0,0 +1,53 @@
+package com.karikeo.cashless.serverrequests;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.util.Map;
+
+public class AsyncSerialToMac extends AsyncRequest {
+
+    private static final String TAG = AsyncSerialToMac.class.getSimpleName();
+    private static final String METHOD_NAME = "GetVendicontMAC";
+
+    final String serial;
+
+    public AsyncSerialToMac(@NonNull final String serial){
+        super(METHOD_NAME);
+        this.serial = serial;
+    }
+
+
+    @Override
+    protected void onExecute(Map<String, String> key_value) {
+        key_value.put(PropertyFields.SERIAL, serial);
+    }
+
+    @Override
+    protected void onPostExecute(Bundle bundle) {
+        if (bundle == null){
+            sendError("Error during request");
+            return;
+        }
+
+        final String mac = bundle.getString(PropertyFields.SERIAL);
+        if (mac == null){
+            sendError("Can not find known parameter");
+            return;
+        }
+
+        Log.d(TAG, "onPostExecute: MAC=" + mac);
+        if (listener != null){
+            listener.OnOk(bundle);
+        }
+    }
+
+    private void sendError(String msg) {
+        Log.d(TAG, "onPostExecute: " + msg);
+        if (listener != null){
+            listener.OnError("Some Error Occur");
+        }
+    }
+}
--- app/src/main/java/com/karikeo/cashless/serverrequests/PropertyFields.java
+++ app/src/main/java/com/karikeo/cashless/serverrequests/PropertyFields.java
@@ -9,7 +9,8 @@ public class PropertyFields {
     public static final String BALANCE = "saldo_actualizado";
     public static final String TRANSACTION_TYPE = "tipoTransaccion";
     public static final String TRANSACTION_BALANCE = "montoTransaccion";
+    public static final String SERIAL = "serie";
 
     public static final String[] ALL_PROPERTIES = {USERID, NAME, PWD, EMAIL, BALANCE,
-        TRANSACTION_TYPE, TRANSACTION_BALANCE};
+        TRANSACTION_TYPE, TRANSACTION_BALANCE, SERIAL};
 }
